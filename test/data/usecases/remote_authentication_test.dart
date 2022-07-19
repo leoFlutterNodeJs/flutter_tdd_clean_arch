3ca096d0e17e7da60a36e5ba535963cd1574c6f0
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-19 15:01:29 -0300

    feat: ensure RemoteAuthentication calls HttpClient with correct body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,6 +1,9 @@
-import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:tdd_clean_arch/domain/usecases/usecases.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -8,30 +11,36 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  Future<void>? auth() async {
-    await httpClient.request(url: url, method: 'post');
+  Future<void>? auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.secret};
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url, required String method});
+  Future<void>? request({required String url, required String method, Map body});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-    late HttpClientSpy httpClient;
-    late String url;
-    late RemoteAuthentication sut;
+  late HttpClientSpy httpClient;
+  late String url;
+  late RemoteAuthentication sut;
 
-  setUp((){
+  setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
   test('Should call HttpClient with correct values', () async {
-    await sut.auth();
-    verify(httpClient.request(url: url, method: 'post'));
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    await sut.auth(params);
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {'email': params.email, 'password': params.secret},
+    ));
   });
 }
