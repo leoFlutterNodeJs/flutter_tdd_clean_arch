2714bb6ace9c25c6ed4ad64b9888360fa2738a58
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-19 19:03:17 -0300

    refactor: move api details from domain entity to data model

---
+++ lib/data/models/remote_account_model.dart
@@ -0,0 +1,11 @@
+import 'package:tdd_clean_arch/domain/entities/account_entity.dart';
+
+class RemoteAccountModel {
+  final String accessToken;
+  RemoteAccountModel(this.accessToken);
+
+  factory RemoteAccountModel.fromJson(Map json) => 
+  RemoteAccountModel(json['accessToken']);
+
+  AccountEntity toEntity() => AccountEntity(accessToken);
+}
