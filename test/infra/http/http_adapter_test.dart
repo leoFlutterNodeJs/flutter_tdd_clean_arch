6932963481d13e1bac2f2d563aad94a97df7cf74
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-29 21:06:43 -0300

    feat: ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -4,6 +4,8 @@ import 'package:mocktail/mocktail.dart';
 
 import 'package:http/http.dart';
 
+import 'package:tdd_clean_arch/data/http/http.dart';
+
 import 'package:tdd_clean_arch/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
@@ -73,6 +75,18 @@ void main() {
       mockResponse(204);
       final response = await sut.request(url: url, method: 'post');
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400 without data', () async {
+      mockResponse(400, body: '');
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
