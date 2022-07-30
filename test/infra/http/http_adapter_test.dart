041e00461a11c4c802d3d9ad47de89e32a13fa6a
leoFlutterNodeJs <leodevnew@gmail.com>
2022-07-29 21:18:43 -0300

    feat: ensure HttpAdapter throws NotFoundError on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -101,6 +101,12 @@ void main() {
       expect(future, throwsA(HttpError.fobidden));
     });
 
+    test('Should return ForbiddenError if post returns 404', () async {
+      mockResponse(404);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('Should return ServerError if post returns 500', () async {
       mockResponse(500);
       final future = sut.request(url: url, method: 'post');
