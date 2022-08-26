552c335526a0a25e943631b42117db87a8c52ab9
leoFlutterNodeJs <leodevnew@gmail.com>
2022-08-26 19:19:38 -0300

    refactor: move pumpWidget to setup method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,24 +3,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_clean_arch/ui/pages/pages.dart';
 
 void main() {
-  testWidgets('Should load with correct email initial state', (WidgetTester tester) async {
+
+  Future<void> loadPage(WidgetTester tester) async {
     const loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets('Should load with correct email initial state', (WidgetTester tester) async {
+    await loadPage(tester);
     final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
     expect(emailTextChildren, findsOneWidget);
   });
 
    testWidgets('Should load with correct password initial state', (WidgetTester tester) async {
-    const loginPage = MaterialApp(home: LoginPage());
-    await tester.pumpWidget(loginPage);
+    await loadPage(tester);
     final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
     expect(passwordTextChildren, findsOneWidget);
   });
 
   testWidgets('Should load with disabled Button', (WidgetTester tester) async {
-    const loginPage = MaterialApp(home: LoginPage());
-    await tester.pumpWidget(loginPage);
+    await loadPage(tester);
     final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
     expect(button.onPressed, null);
+  });
+
+  testWidgets('Should call validate with correct values', (WidgetTester tester) async {
+    await loadPage(tester);
+    final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+    expect(passwordTextChildren, findsOneWidget);
   });
 }
