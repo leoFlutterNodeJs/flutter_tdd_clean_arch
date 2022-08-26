348df2b6e100ce35b747b8dc2e216ffd34da5f6b
leoFlutterNodeJs <leodevnew@gmail.com>
2022-08-26 19:06:42 -0300

    test: add correct initial state to Email

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_clean_arch/ui/pages/pages.dart';
+
+void main() {
+  testWidgets('Should load with correct email initial state', (WidgetTester tester) async {
+    const loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+    final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
+    expect(emailTextChildren, findsOneWidget);
+  });
+}
