cdbbe54c62f9144d9ba53961fde6d19dfe9bed2e
leoFlutterNodeJs <leodevnew@gmail.com>
2022-09-08 09:22:23 -0300

    feat: ensure EmailValidaton returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,25 @@
+import 'package:test/test.dart';
+
+import 'package:tdd_clean_arch/validation/protocols/protocols.dart';
+
+class EmailValidaton implements FieldValidation {
+  @override
+  final String field;
+
+  EmailValidaton(this.field);
+
+  @override
+  String? validate(String? value) {
+    return null;
+  }
+}
+
+void main() {
+  
+  test('Should returns null if email is empty', () {
+    final sut = EmailValidaton('any_field');
+    final error = sut.validate('');
+
+    expect(error, null);
+  });
+}
