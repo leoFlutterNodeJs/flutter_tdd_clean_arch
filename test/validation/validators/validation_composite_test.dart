946a1bdcdf1f513fdcb682c918f8406d90327e29
leoFlutterNodeJs <leodevnew@gmail.com>
2022-09-08 16:12:45 -0300

    refactor: move ValidationComposite to own file

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,27 +1,9 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import 'package:tdd_clean_arch/presentation/protocols/protocols.dart';
-
 import 'package:tdd_clean_arch/validation/protocols/protocols.dart';
+import 'package:tdd_clean_arch/validation/validators/validators.dart';
 
-class ValidationComposite implements Validation {
-  final List<FieldValidation> validations;
-
-  ValidationComposite(this.validations);
-  
-  @override
-  String? validate({required String field, required String value}) {
-    String? error;
-    for (final validation in validations.where((v) => v.field == field)) {
-      error = validation.validate(value);
-      if(error != null && error.isNotEmpty == true) {
-        return error;
-      }
-    }
-    return error;
-  }
-}
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
