179b17780b5b5c35721af3bd568eaeebeab4da7b
leoFlutterNodeJs <leodevnew@gmail.com>
2022-09-19 14:55:20 -0300

    feat: ensure LocalSaveCurrentAccount calls SaveSecureCacheStorage with correct values

---
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -0,0 +1,33 @@
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import 'package:tdd_clean_arch/domain/usecases/usecases.dart';
+import 'package:tdd_clean_arch/domain/entities/entities.dart';
+
+class LocalSaveCurrentAccount implements SaveCurrentAccount {
+  final SaveSecureCacheStorage saveSecureCacheStorage;
+
+  LocalSaveCurrentAccount({required this.saveSecureCacheStorage});
+
+  @override
+  Future<void>? save(AccountEntity account) async {
+    await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+  }
+}
+
+abstract class SaveSecureCacheStorage {
+  Future<void>? saveSecure({required String key, required String value});
+}
+
+class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
+
+void main() {
+  test('Should call CacheStorage with correct values', () async {
+    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    final account = AccountEntity(faker.guid.guid());
+    await sut.save(account);
+    verify(() => saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
+  });
+}
