6313da6ef0ae4d063a42f6cad8078fc506aaaca0
leoFlutterNodeJs <leodevnew@gmail.com>
2022-10-11 10:55:51 -0300

    refactor: move LocalStorageAdapter to its own file

--- test/infra/http/cache/local_storage_adapter_test.dart
+++ test/infra/http/cache/local_storage_adapter_test.dart
@@ -1,27 +1,18 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
-import 'package:tdd_clean_arch/data/cache/cache.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-
-  LocalStorageAdapter({required this.secureStorage});
-
-  @override
-  Future<void>? saveSecure({required String key, required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
+import 'package:tdd_clean_arch/infra/cache/cache.dart';
 
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {
   FlutterSecureStorageSpy() {
     When mockDeleteCall() => when(() => delete(key: any(named: 'key')));
     void mockDelete() => mockDeleteCall().thenAnswer((_) async => _);
-    When mockSaveCall() => when(() => write(key: any(named: 'key'), value: any(named: 'value')));
-    void mockSave() =>mockSaveCall().thenAnswer((_) async => _);
+    When mockSaveCall() =>
+        when(() => write(key: any(named: 'key'), value: any(named: 'value')));
+    void mockSave() => mockSaveCall().thenAnswer((_) async => _);
 
     mockDelete();
     mockSave();
@@ -33,7 +24,7 @@ void main() {
   late FlutterSecureStorageSpy secureStorage;
   late String key, value;
 
-  setUp((){
+  setUp(() {
     key = faker.lorem.word();
     value = faker.guid.guid();
     secureStorage = FlutterSecureStorageSpy();
@@ -41,9 +32,10 @@ void main() {
   });
 
   void mockSaveSecureError() {
-  when(()=> secureStorage.write(key: any(named: 'key'), value: any(named: 'value')))
-    .thenThrow(Exception());
-}
+    when(() => secureStorage.write(
+        key: any(named: 'key'),
+        value: any(named: 'value'))).thenThrow(Exception());
+  }
 
   test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
